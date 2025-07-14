3e5c49e103d4322c475f7aa0a79c4f5dd581226f
PriyankaNigade18 <piyu1818@gmail.com>
2025-07-14 08:36:06 +0530

    WebDriver day-19

--- src/test/java/com/WebTesting_Alert_Frame/S38_NestedFRame.java
+++ src/test/java/com/WebTesting_Alert_Frame/S38_NestedFRame.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 
 public class S38_NestedFRame {
   @Test
-  public void f() 
+  public void testFrame() 
   {
 	  WebDriver driver=new ChromeDriver();
 	  driver.get("https://the-internet.herokuapp.com/nested_frames");
@@ -17,6 +17,21 @@ public class S38_NestedFRame {
 	  driver.switchTo().frame(0).switchTo().frame("frame-left");
 	  System.out.println(driver.getPageSource());
 	  
+	  //left to middle
+	  driver.switchTo().parentFrame().switchTo().frame("frame-middle");
+	  System.out.println(driver.getPageSource());
+	  
+	  //middle to right
+	  driver.switchTo().parentFrame().switchTo().frame("frame-right");
+	  System.out.println(driver.getPageSource());
+	  
+	  //bottom
+	  driver.switchTo().defaultContent().switchTo().frame(1);
+	  System.out.println(driver.getPageSource());
+	  
+	  
+	 
+	 
 	  
 	  
   }
