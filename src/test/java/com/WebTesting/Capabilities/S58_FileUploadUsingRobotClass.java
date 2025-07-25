58636bc89dae8272e75d2eaaad0164590e4e453c
PriyankaNigade18 <piyu1818@gmail.com>
2025-07-25 08:50:11 +0530

    WebDriver Day-27

--- src/test/java/com/WebTesting/Capabilities/S58_FileUploadUsingRobotClass.java
+++ src/test/java/com/WebTesting/Capabilities/S58_FileUploadUsingRobotClass.java
@@ -77,7 +77,7 @@ public class S58_FileUploadUsingRobotClass {
 	   //upload
 	   driver.findElement(By.id("file-submit")).click();
 	   
-	   Thread.sleep(2000);
+	   Thread.sleep(4000);
 	   /*
 	   //validate mesg
 	   String exp="File Uploaded!";
@@ -90,8 +90,8 @@ public class S58_FileUploadUsingRobotClass {
 	   {
 		   System.out.println("Test Fail....File not uploaded!");
 	   }
-	   */
-	  
+	   
+	  */
 	  
 	  
 	  
