410899063e2f912aadc1e58c7ff1eb92121fe1f0
PriyankaNigade18 <piyu1818@gmail.com>
2025-09-16 08:34:40 +0530

    ChainTest Report added

--- src/test/java/com/WebTesting/TestNG/TestListener/BaseClass.java
+++ src/test/java/com/WebTesting/TestNG/TestListener/BaseClass.java
@@ -1,8 +1,14 @@
 package com.WebTesting.TestNG.TestListener;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
+import com.aventstack.chaintest.plugins.ChainTestListener;
 
 public class BaseClass 
 {
@@ -15,6 +21,18 @@ public class BaseClass
 		driver=new EdgeDriver();
 		driver.get("https://automationplayground.com/crm/");
 		
+	}
+	
+	//attached screenshot for failure
+	@AfterMethod
+	public void getScreenshot(ITestResult result)
+	{
+		if(!result.isSuccess())
+		{
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		byte arr[]=ts.getScreenshotAs(OutputType.BYTES);
+		ChainTestListener.embed(arr,"image/png");
+		}
 	}
 
 }
--- src/test/java/com/WebTesting/TestNG/TestListener/TestCRMApp.java
+++ src/test/java/com/WebTesting/TestNG/TestListener/TestCRMApp.java
@@ -1,11 +1,14 @@
 package com.WebTesting.TestNG.TestListener;
 
 import org.openqa.selenium.By;
+import com.aventstack.chaintest.plugins.ChainTestListener;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 //@Listeners(MyListener.class)
+@Listeners(ChainTestListener.class)
 public class TestCRMApp extends BaseClass  
 {
   @Test(priority=1)
@@ -15,21 +18,27 @@ public class TestCRMApp extends BaseClass
 	  if(link.isDisplayed() && link.isEnabled())
 	  {
 		  link.click();
+		  ChainTestListener.log("SignIn Link is working.... ");
 	  }
+	  
   }
   
   @Test(priority=2)
   public void testLogin() 
   {
+	  ChainTestListener.log("Login Test is executing..... ");
 	  driver.findElement(By.id("email-id")).sendKeys("test@gmail.com");
 	  driver.findElement(By.id("password")).sendKeys("test123");
 	  driver.findElement(By.id("submit-Button")).click();
+	  Assert.assertTrue(driver.getCurrentUrl().contains("customers"));
+	  ChainTestListener.log("Login completed!...... ");
+	  
   
   }
   
   @Test(priority=3)
   public void testLogout() 
-  {
+  { 	ChainTestListener.log("Logout Test is executing..... ");
 	  driver.findElement(By.linkText("Sign Out")).click();
   }
 }
